1dacb320f78b889da80045699e15a26d2aa99a33
anorld-droid <mulindipatrice00@gmail.com>
2022-10-09 11:35:56 +0300

    Changed root page to login/signin screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:trice/views/routing/routes.dart';
+import 'package:trice/views/viewscreens/authentication/sign_in.dart';
 import 'package:trice/views/viewscreens/authentication/sign_up.dart';
-import 'package:trice/views/viewscreens/home_screen.dart';
 
 void main() => runApp(GetMaterialApp(
       initialRoute: root,
       getPages: [
-        GetPage(name: root, page: () => const Home()),
+        GetPage(name: root, page: () => const SignIn()),
         GetPage(name: signup, page: () => const SignUp())
       ],
     ));
