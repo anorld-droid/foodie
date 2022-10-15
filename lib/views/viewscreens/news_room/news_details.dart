3b1b7e4a84443b1247d699e0dcb4b015480b104d
anorld-droid <mulindipatrice00@gmail.com>
2022-10-15 13:37:03 +0300

    Created picture header , title and subtitle, with paragraph formating.

--- lib/views/viewscreens/news_room/news_details.dart
+++ lib/views/viewscreens/news_room/news_details.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
 import 'package:trice/model/news/news_post.dart';
-import 'package:trice/views/viewscreens/news_room/widgets.dart';
+import 'package:trice/views/viewscreens/news_room/widgets/news.dart';
 
 class NewsDetail extends StatelessWidget {
   final NewsPostModel newsPostModel;
@@ -11,14 +12,71 @@ class NewsDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DetailsTopBar(
-          title: newsPostModel.title,
-          profilePhoto: "newsPostModel.metadata.author.profilePhoto"),
-      body: Center(
-        child: Text(
-          newsPostModel.title,
-        ),
-      ),
-    );
+        appBar: DetailsTopBar(
+            title: newsPostModel.publication?.name ?? "",
+            profilePhoto: "newsPostModel.metadata.author.profilePhoto"),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: Get.width,
+                height: 100,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                )),
+                child: Image.network(newsPostModel.imageId),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                newsPostModel.title,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              newsPostModel.subtitle != null
+                  ? SizedBox(
+                      height: 16,
+                      child: Text(
+                        newsPostModel.subtitle ?? "",
+                      ),
+                    )
+                  : const SizedBox(
+                      height: 2,
+                    ),
+              Row(
+                children: [
+                  Container(
+                    height: 19.47,
+                    width: 21.33,
+                    decoration: BoxDecoration(
+                        color: Get.theme.primaryColorDark.withOpacity(0.4),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(100))),
+                    child: const Icon(Icons.account_circle),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Text(newsPostModel.metadata.author.name),
+                      Text(
+                          "${newsPostModel.metadata.date} · ${newsPostModel.metadata.readTimeMinutes} mins")
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/views/viewscreens/news_room/widgets/details.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trice/model/news/news_post.dart';
+
+class ParagraphCard extends StatelessWidget {
+  final Paragraph paragraph;
+  const ParagraphCard({Key? key, required this.paragraph}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ParagraphStyling styling = paragraph.type.getTextAndParagraphStyle();
+    return Column(
+      children: const [],
+    );
+  }
+}
+
+extension on ParagraphType {
+  ParagraphStyling getTextAndParagraphStyle() {
+    TextStyle textStyle = Get.textTheme.bodyMedium ?? TextStyle();
+    ParagraphStyle paragraphStyle = ParagraphStyle(textIndent: 10);
+    var trailingPadding = 24;
+    switch (this) {
+      case ParagraphType.caption:
+        textStyle = Get.textTheme.labelMedium!;
+        break;
+      case ParagraphType.title:
+        textStyle = Get.textTheme.headlineLarge!;
+        break;
+      case ParagraphType.subHead:
+        textStyle = Get.textTheme.headlineSmall!;
+        trailingPadding = 16;
+        break;
+      case ParagraphType.text:
+        textStyle = Get.textTheme.bodyLarge!.copyWith(height: 28);
+        break;
+      case ParagraphType.header:
+        textStyle = Get.textTheme.headlineMedium!;
+        trailingPadding = 16;
+        break;
+
+      case ParagraphType.codeBlock:
+        textStyle = Get.textTheme.headline1!;
+        break;
+      case ParagraphType.quote:
+        textStyle = Get.textTheme.bodyLarge!;
+        break;
+      case ParagraphType.bullet:
+        paragraphStyle = ParagraphStyle(textIndent: 8);
+        break;
+      default:
+    }
+
+    return ParagraphStyling(
+      textStyle: textStyle,
+      paragraphStyle: paragraphStyle,
+      trailingPadding: trailingPadding,
+    );
+  }
+}
