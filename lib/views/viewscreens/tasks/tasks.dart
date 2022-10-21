8d8cab093e9cf86468836288a0a5d12239f82cf2
anorld-droid <mulindipatrice00@gmail.com>
2022-10-21 13:26:14 +0300

    Change from statelesswidget to Getview to use taskcontroller for the screen implementation.

--- lib/views/viewscreens/tasks/tasks.dart
+++ lib/views/viewscreens/tasks/tasks.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:trice/controller/task_controller.dart';
+import 'package:trice/model/data/tasks.dart';
 
-class Tasks extends StatelessWidget {
+class Tasks extends GetView<TaskController> {
   const Tasks({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Tasks"),
+    Get.put(
+      TaskController(
+        filters: tasks.filters,
       ),
     );
+    return Scaffold(
+        body: Column(
+      children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: controller.filterWidgets,
+          ),
+        )
+      ],
+    ));
   }
 }
