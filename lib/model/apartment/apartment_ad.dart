de15e8b27dd912e3055695e7ae08a0c402f9f1b8
anorld-droid <mulindipatrice00@gmail.com>
2022-10-21 12:03:55 +0300

    add apartment contact details.

--- lib/model/apartment/apartment_ad.dart
+++ lib/model/apartment/apartment_ad.dart
@@ -4,10 +4,12 @@ class ApartmentAdModel {
   final String location;
   final String priceRange;
   final int unOccupied;
+  final int contact;
   ApartmentAdModel(
       {required this.unOccupied,
       required this.priceRange,
       required this.imagesUrl,
       required this.name,
+      this.contact = 757913481,
       required this.location});
 }
