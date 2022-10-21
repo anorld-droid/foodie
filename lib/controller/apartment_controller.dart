8f016f64760d31c1611def015edfb0089f5a23fc
anorld-droid <mulindipatrice00@gmail.com>
2022-10-21 12:02:55 +0300

    open whatsapp when inquire button on search apartment screen click.

--- lib/controller/apartment_controller.dart
+++ lib/controller/apartment_controller.dart
@@ -1,3 +1,21 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class ApartmentController extends GetxController {}
+class ApartmentController extends GetxController {
+  TextEditingController searchController = TextEditingController();
+  var searching = false.obs;
+  inquire(int phoneNumber) async {
+    String url = "https://wa.me/+254$phoneNumber?text=Apartment Inquiry, ";
+    Uri urlEncoded = Uri.parse(url);
+    if (await canLaunchUrl(urlEncoded)) {
+      await launchUrl(urlEncoded);
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+
+  performSearch() {
+    searching.toggle();
+  }
+}
