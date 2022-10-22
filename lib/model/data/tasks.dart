f6eef0a26848652c008dd7956b818c54419cd5e6
anorld-droid <mulindipatrice00@gmail.com>
2022-10-22 07:32:33 +0300

    Map task to date as key.

--- lib/model/data/tasks.dart
+++ lib/model/data/tasks.dart
@@ -1,13 +1,11 @@
 import 'package:trice/model/tasks.dart';
 
-var tasks = TaskModel(
-  filters: [
-    "Filter1",
-    "Filter2",
-    "Filter3",
-    "Filter4",
-    "Filter5",
-    "Filter6",
-    "Filter7",
-  ],
-);
+var tasks = TaskModel(filters: [
+  "Filter1",
+  "Filter2",
+  "Filter3",
+  "Filter4",
+  "Filter5",
+  "Filter6",
+  "Filter7",
+], tasks: {});
--- lib/model/tasks.dart
+++ lib/model/tasks.dart
@@ -1,5 +1,13 @@
 class TaskModel {
   final List<String> filters;
+  final Map<String, TaskItem> tasks; // Use date as the key
 
-  TaskModel({required this.filters});
+  TaskModel({required this.tasks, required this.filters});
+}
+
+class TaskItem {
+  final String text;
+  final bool done;
+
+  TaskItem({required this.text, required this.done});
 }
