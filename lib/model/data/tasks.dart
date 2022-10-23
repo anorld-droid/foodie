01c82ab1d293c0f694ebae36174a195f03d421bf
anorld-droid <mulindipatrice00@gmail.com>
2022-10-23 15:40:46 +0300

    Sample tasks for development/Testing purposes.

--- lib/model/data/tasks.dart
+++ lib/model/data/tasks.dart
@@ -8,4 +8,25 @@ var tasks = TaskModel(filters: [
   "Filter5",
   "Filter6",
   "Filter7",
-], tasks: {});
+], tasks: {
+  "Oct 22, Sunday": [
+    TaskItem(time: "7:30pm", text: "Lorem ipsum dolor sit amet.", done: false),
+    TaskItem(
+        time: "8:30am",
+        text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, magna dignissim massa sed etiam felis amet, ultrices. ",
+        done: true)
+  ],
+  "Oct 23, Monday": [
+    TaskItem(
+        time: "9:30am",
+        text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, magna dignissim massa sed etiam felis amet, ultrices. ",
+        done: false),
+    TaskItem(
+        time: "8:30am",
+        text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, magna dignissim massa sed etiam felis amet, ultrices. ",
+        done: false)
+  ],
+});
