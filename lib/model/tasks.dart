e0db150c98aa3482cf7a3f3281ad164be3690bca
anorld-droid <mulindipatrice00@gmail.com>
2022-10-23 15:40:09 +0300

    Show time when the task is due.

--- lib/model/tasks.dart
+++ lib/model/tasks.dart
@@ -1,13 +1,14 @@
 class TaskModel {
-  final List<String> filters;
-  final Map<String, TaskItem> tasks; // Use date as the key
+  List<String> filters;
+  Map<String, List<TaskItem>> tasks; // Use date as the key
 
   TaskModel({required this.tasks, required this.filters});
 }
 
 class TaskItem {
-  final String text;
-  final bool done;
+  String text;
+  String time;
+  bool done;
 
-  TaskItem({required this.text, required this.done});
+  TaskItem({required this.time, required this.text, required this.done});
 }
