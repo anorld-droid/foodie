453fa35ba348f97b02217308821fcdfdd10f56d0
anorld-droid <mulindipatrice00@gmail.com>
2022-11-12 09:45:43 +0300

    More fields to the model.

--- lib/model/events/events.dart
+++ lib/model/events/events.dart
@@ -1,7 +1,23 @@
 import 'package:trice/model/news/news_post.dart';
 
 class EventModel {
-  final List<String> imageUrls;
   final Metadata metadata;
-  EventModel({required this.metadata, required this.imageUrls});
+  final List<String> imageUrls;
+  final String commentsLength;
+  final String likesLength;
+  final List<Comment> comments;
+  final List<Metadata> likes;
+  final String rating;
+  final String attendees;
+  final String description;
+  EventModel(
+      {required this.metadata,
+      required this.imageUrls,
+      required this.commentsLength,
+      required this.likesLength,
+      required this.likes,
+      required this.comments,
+      required this.rating,
+      required this.attendees,
+      required this.description});
 }
